c6f506dd32b1c14036554630e17dddf9239748a3
cris-c-castillo <ccarri11@ucsc.com>
2020-05-31 15:45:10 -0700

    fixed nanos landing page all responsive

--- src/components/Nano.js
+++ src/components/Nano.js
@@ -12,32 +12,101 @@ import NanoEvaporation from './NanoEvaporation.js';
 import Nano3D from './nano3D.js'
 import FlipCard from './NanoCardFlip'
 import FlipCardTwo from './NanoCardFlipTwo'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Container from 'react-bootstrap/Container'
+import Jumbotron from 'react-bootstrap/Jumbotron'
+import Fade from "react-reveal/Fade";
 
 const Nano = () => {
 
 
-  const styleJumbo = {
-    backgrondColor:'black'
-  }
+  const styleFooter = {
+    width: '100%',
+    marginTop: '-5.6%',
+    height: '38vh'
+  };
+
   const styleBack = {
     backgrondColor:'black'
   }
 
   const oceanFormat = {
-    width:'100%',
-    height:'80vh'
-  }
+    minInlineSize:'-webkit-fill-available',
+    maxHeight:'fit-content',
+    marginTop:'-16.5%',
+    height:'130vh'
+
+
+  };
+
   const oceanFormatTwo = {
-    position:'relative',
     width:'100%',
-    marginTop:'-3%',
-    height:'35vh'
   }
+
+  const newAlignmentCards = {
+      position:'absolute',
+      marginTop: '13%',
+      backgroundColor:'transparent',
+      width: '100%',
+      display: 'table-header-group'
+    
+  };
 
   return (
     <div>
-      <NanoEvaporation />
-       <Spring from ={{ opacity: 0, marginTop: -500}} to ={{ opacity: 1, marginTop:0 }}>
+      <div style = {{backgroundColor:'black'}}>
+
+        <Fade bottom>
+          <h1 style = {{height:'20vh',fontSize:'70px',fontFamily:'sans-serif',color:'white'}}>Nanoparticles</h1>
+        </Fade>
+      </div>
+
+      <div>
+        <Jumbotron
+          fluid
+          style={{
+            display: "flex",
+            placeContent: "center",
+          }}
+        >
+          <Container style={newAlignmentCards}>
+            <Row>
+              <Col>
+                <FlipCard />
+              </Col>
+              <Col>
+                <FlipCardTwo />
+              </Col>
+            </Row>
+          </Container>
+        </Jumbotron>
+      </div>
+      <div>
+        <img
+          style={oceanFormat}
+          src="https://cdn.downtoearth.org.in/library/large/2019-12-04/0.92334300_1575475699_17.jpg"
+        />
+      </div>
+      <div>
+        <Jumbo />
+
+        <img
+          src="https://singularityhub.com/wp-content/uploads/2019/08/clear-concept-underwater-ocean-floor-perspectives-shutterstock-1422884786.jpg"
+          style={styleFooter}
+        />
+      </div>
+    </div>
+  );
+}
+
+
+export default Nano;
+
+
+/*
+ <NanoEvaporation />
+   <Spring from ={{ opacity: 0, marginTop: -500}} to ={{ opacity: 1, marginTop:0 }}>
         {props => (
           <div style= {props}>
 
@@ -46,27 +115,11 @@ const Nano = () => {
 
             <div className="backgroundNano parallax">
             </div>
-             <NanoComponentTwo />
-             <FlipCardTwo />
-             <FlipCard />
-             <img src = "https://cdn.downtoearth.org.in/library/large/2019-12-04/0.92334300_1575475699_17.jpg" style = {oceanFormat}/>
 
-            <div className ="nanoInfo">
-            </div>
-
-              <Jumbo className = 'styleJumbo' />
-              <img src = "https://singularityhub.com/wp-content/uploads/2019/08/clear-concept-underwater-ocean-floor-perspectives-shutterstock-1422884786.jpg" style = {oceanFormatTwo}/>
-
-            </div>
-        )}
+   )}
        </Spring>
 
 
+       
 
-       </div>
-
-  );
-}
-
-
-export default Nano;
+*/
